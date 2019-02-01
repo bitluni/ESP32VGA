22afdebc706e4290b6ef933c4b0cb2b7e72d127b
bitluni <bitluni@gmail.com>
2019-02-01 15:26:46 +0100

    vga example

--- ESP32VGA.ino
+++ ESP32VGA.ino
@@ -2,13 +2,16 @@
 #include "src/ESP32Lib/src/Graphics/I2SVGA.h"
 #include <soc/rtc.h>
 #include "src/ESP32Lib/src/Graphics/Mesh.h"
+#include "src/ESP32Lib/src/Graphics/Sprites.h"
 
 typedef unsigned short Color;
 
-#include "font6x8.h"
-#include "venus.h"
+#include "gfx/font6x8.h"
+#include "gfx/thinker.h"
+#include "gfx/venus.h"
 Font<Graphics<Color>> font(6, 8, font6x8::pixels);
 Mesh<Graphics<Color>> model(venus::vertexCount, venus::vertices, 0, 0, venus::triangleCount, venus::triangles, venus::triangleNormals);
+Mesh<Graphics<Color>> model2(thinker::vertexCount, thinker::vertices, 0, 0, thinker::triangleCount, thinker::triangles, thinker::triangleNormals);
 
 //Note that the I/O GPIO pads are 0-19, 21-23, 25-27, 32-39, while the output GPIOs are 0-19, 21-23, 25-27, 32-33. GPIO pads 34-39 are input-only
 //0, 2, 4, 5, 12,  13, 14, 15, 16,  17, 18, 19, 21,  22, 23, 27, 32, 33
@@ -29,51 +32,51 @@ I2SVGA graphics(0);
 
 void setup()
 {
-	Serial.begin(115200);
-	rtc_clk_cpu_freq_set_fast(RTC_CPU_FREQ_240M); //highest cpu frequency
-	//initializing i2s vga and frame buffers
-	graphics.init(graphics.MODE320x120, redPins, greenPins, bluePins, hsyncPin, vsyncPin);
-	//init(XRES, YRES, redPins, greenPins, bluePins, hsyncPin, vsyncPin);
-	//select font
-	graphics.setFont(font);
+  Serial.begin(115200);
+  //initializing i2s vga and frame buffers
+  graphics.init(graphics.MODE200x150, redPins, greenPins, bluePins, hsyncPin, vsyncPin, 16);
+  //select font
+  graphics.setFont(font);
 }
 
 void drawLogo()
 {
-	static Matrix perspective = Matrix::translation(graphics.xres / 2, graphics.yres / 2, 0) * Matrix::scaling(100 * graphics.pixelAspect(), 100, 100) * Matrix::perspective(90, 1, 10);
-	static float u = 0;
-	u += 0.02;
-	Matrix rotation = Matrix::rotation(-1.7, 1, 0, 0) * Matrix::rotation(u, 0, 0, 1);
-	Matrix m0 = perspective * Matrix::translation(0, 1.7 * 0, 7) * rotation * Matrix::scaling(7);
-	model.transform(m0, rotation);
-	model.drawTriangles(graphics, 31);
-	graphics.flush();
+  static Matrix perspective = Matrix::translation(graphics.xres / 2, graphics.yres / 2, 0) * Matrix::scaling(100 * graphics.pixelAspect(), 100, 100) * Matrix::perspective(90, 1, 10);
+  static float u = 0;
+  u += 0.02;
+  Matrix rotation = Matrix::rotation(-1.7, 1, 0, 0) * Matrix::rotation(u, 0, 0, 1);
+  Matrix m0 = perspective * Matrix::translation(0, 1.7 * 0, 7) * rotation * Matrix::scaling(7);
+  model2.transform(m0, rotation);
+  model2.drawTriangles(graphics, 31);
+  graphics.flush();
 }
 
 void draw()
 {
-	static int lastMillis = 0;
-	int t = millis();
-	int fps = 1000 / (t - lastMillis);
-	lastMillis = t;
-	graphics.begin();
-	//graphics.clear((16) | ((32) << 5) | ((16) << 11));
-	for (int y = 0; y < graphics.yres; y++)
-		for (int x = 0; x < graphics.xres; x++)
-			graphics.backbuffer[y][x] = 0; //(31 - x / 8) | ((16) << 5) | ((15 - y / 16) << 10);
-	drawLogo();
-	graphics.setTextColor(0xffff);
-	graphics.setCursor(0, 0);
-	graphics.print("mem: ");
-	graphics.print((int)heap_caps_get_free_size(MALLOC_CAP_DEFAULT));
-	graphics.print(" fps: ");
-	graphics.print(fps, 10, 2);
-	graphics.print(" tris/s: ");
-	graphics.print(fps * model.triangleCount);
-	graphics.end();
+  static int lastMillis = 0;
+  int t = millis();
+  int fps = 1000 / (t - lastMillis);
+  lastMillis = t;
+  graphics.begin();
+  //graphics.clear((16) | ((32) << 5) | ((16) << 11));
+  for (int y = 0; y < graphics.yres; y++)
+    for (int x = 0; x < graphics.xres; x++)
+      graphics.backbuffer[y][x] = 0; //(31 - x / 8) | ((16) << 5) | ((15 - y / 16) << 10);
+  drawLogo();
+  //luni.drawMix(graphics, (t / 100) % 5, 30, 0);
+  graphics.setTextColor(0xffff);
+  graphics.setCursor(0, 0);
+  //graphics.print("mem: ");
+  //graphics.print((int)heap_caps_get_free_size(MALLOC_CAP_DEFAULT));
+  //graphics.print(" fps: ");
+  //graphics.print(fps, 10, 2);
+  //graphics.print(" tris/s: ");
+  //graphics.print(fps * model.triangleCount);
+  graphics.end();
 }
 
 void loop()
 {
-	draw();
+  draw();
+  //delay(20);
 }
